1be38d2f2f70c20a172668746ac0c2e38188b6c3
joelmf06 <joelfernandez06@gmail.com>
2021-05-09 03:17:53 -0500

    firebase registro

--- app/src/main/java/com/joelmf06/Jpayment/HomePage.kt
+++ app/src/main/java/com/joelmf06/Jpayment/HomePage.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import com.joelmf06.Jpayment.databinding.ActivityHomePageBinding
 
 class HomePage : AppCompatActivity() {
+
     private lateinit var binding: ActivityHomePageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomePageBinding.inflate(layoutInflater)
--- app/src/main/java/com/joelmf06/Jpayment/MainActivity.kt
+++ app/src/main/java/com/joelmf06/Jpayment/MainActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import com.joelmf06.Jpayment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
