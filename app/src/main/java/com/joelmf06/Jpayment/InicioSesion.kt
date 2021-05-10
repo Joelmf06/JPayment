705c5444fe22dd9269ec545910a3895bf1bdba9b
joelmf06 <joelfernandez06@gmail.com>
2021-05-10 00:07:53 -0500

    Logica botones

--- app/src/main/java/com/joelmf06/Jpayment/InicioSesion.kt
+++ app/src/main/java/com/joelmf06/Jpayment/InicioSesion.kt
@@ -3,21 +3,60 @@ package com.joelmf06.Jpayment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 import com.joelmf06.Jpayment.databinding.ActivityInicioSesionBinding
 
 class InicioSesion : AppCompatActivity() {
+
     private lateinit var binding: ActivityInicioSesionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityInicioSesionBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        click_BtnLogin ()
+
+        setup ()
 
     }
-    fun click_BtnLogin () {
+    private fun setup () {
+        title = "Inicio Sesión"
+
         binding.btnLogin.setOnClickListener {
-            val btnIngreso: Intent = Intent (applicationContext, HomePage::class.java)
-            startActivity(btnIngreso)
+            if (binding.etLoginAddress.text.isNotEmpty() && binding.etLoginPassword.text.isNotEmpty()) {
+
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(binding.etLoginAddress.text.toString(),
+                        binding.etLoginPassword.text.toString()).addOnCompleteListener {
+
+                        if (it.isSuccessful) {
+                            showLogin(it.result?.user?.email?: "")
+                        }else {
+                            showAlert()
+                        }
+
+                    }
+            }
         }
+
     }
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton(text: "Aceptar", listener:null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showLogin (email: String, password: String) {
+
+        val LoginIntent: Intent = Intent (this, HomePage::class.java).apply { this
+            putExtra("email", email)
+            putExtra("password", password)
+        }
+        startActivity(LoginIntent)
+
+    }
+
 }
--- app/src/main/java/com/joelmf06/Jpayment/MainActivity.kt
+++ app/src/main/java/com/joelmf06/Jpayment/MainActivity.kt
@@ -3,7 +3,11 @@ package com.joelmf06.Jpayment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.ActionCodeEmailInfo
+import com.google.firebase.auth.FirebaseAuth
 import com.joelmf06.Jpayment.databinding.ActivityMainBinding
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,15 +17,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        click_BtnRegistro ()
+        //Setup
+        setup ()
+
+    }
+     private fun setup () {
+         title = "Autenticacíón"
+
+         binding.btnRegistro.setOnClickListener {
+             if (binding.etEmailAddress.text.isNotEmpty() && binding.etPassword.text.isNotEmpty()) {
+
+                 FirebaseAuth.getInstance()
+                     .createUserWithEmailAndPassword(binding.etEmailAddress.text.toString(),
+                     binding.etPassword.text.toString()).addOnCompleteListener {
+
+                         if (it.isSuccessful) {
+                             showLogin(it.result?.user?.email ?: "")
+                         }else {
+                             showAlert()
+                         }
+
+                 }
+             }
+         }
+
+     }
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton( text: "Aceptar", listener:null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 
-    fun click_BtnRegistro () {
-        binding.btnRegistro.setOnClickListener {
-            /*Toast.makeText(applicationContext, "Campos obligatorios", Toast.LENGTH_SHORT).show()*/
-            val btnInit: Intent = Intent (applicationContext, InicioSesion::class.java)
-            startActivity(btnInit)
-        }
+    private fun showLogin (email: String, password: String) {
+
+            val LoginIntent: Intent = Intent (this, InicioSesion::class.java).apply { this
+            putExtra("email", email)
+            putExtra("password", password)
+            }
+            startActivity(LoginIntent)
+
     }
+
 
 }
